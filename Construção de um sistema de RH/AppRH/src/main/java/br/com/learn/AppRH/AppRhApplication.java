d0e2227261069f0eb9701d04474beb9f9360cbd8
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-03-05 08:08:54 -0300

    Iniciando Projeto

---
+++ Construção de um sistema de RH/AppRH/src/main/java/br/com/learn/AppRH/AppRhApplication.java
@@ -0,0 +1,13 @@
+package br.com.learn.AppRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRhApplication.class, args);
+	}
+
+}
---
+++ Construção de um sistema de RH/AppRH/src/test/java/br/com/learn/AppRH/AppRhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learn.AppRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
